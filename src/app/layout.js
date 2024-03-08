fc6067a02ffed4ff4261c7d8fca64a9ac2dc1691
Ioannis H <ihdba@yahoo.com>
2024-03-08 08:50:02 +0100

    Added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from '@/components/Header'
-
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Header />
-        {children}</body>
+        <Providers>
+          <Header />
+            {children}
+        </Providers>        
+      </body>
     </html>
   );
 }
