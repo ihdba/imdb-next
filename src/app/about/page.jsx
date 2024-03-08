add166dcb92a598d6b340e14fa33caa64ea08743
Ioannis H <ihdba@yahoo.com>
2024-03-08 09:08:16 +0100

    Created About page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p className='text-lg text-gray-700 dark:text-gray-200'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis maiores numquam id et alias molestiae similique labore illum explicabo quis.
+        </p>
+
+        <p className='text-lg text-gray-700 dark:text-gray-200'>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi qui, veniam voluptatem repellat et inventore, assumenda quaerat quae fugit facilis deserunt corporis officia, unde rerum amet. Dolorum dignissimos consequatur quasi?
+        </p>
+
+        <p className='text-lg text-gray-700 dark:text-gray-200'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem nisi sit excepturi adipisci accusantium perferendis voluptatum molestiae velit, ad quidem, animi ex saepe aspernatur. Suscipit dolores cum maxime magnam quo.
+        </p>
+    </div>
+  )
+}
--- src/components/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch.jsx
@@ -14,7 +14,16 @@ export default function DarkModeSwitch() {
   return (
     <div> 
       
-        {mounted && (currentTheme === 'dark' ? (<MdLightMode onClick={() => setTheme('light')} className='text-xl cursor-pointer hover:text-amber-500' /> ): (<MdDarkMode onClick={() => setTheme('dark')} className='text-xl cursor-pointer hover:text-amber-500'/>))}
+        {mounted && (currentTheme === 'dark' ? (
+          <MdLightMode
+             onClick={() => setTheme('light')} 
+             className='text-xl cursor-pointer hover:text-amber-500' /> 
+             ): (
+             <MdDarkMode 
+             onClick={() => setTheme('dark')} 
+             className='text-xl cursor-pointer hover:text-amber-500'
+             />
+             ))}
 
     </div>
   )
